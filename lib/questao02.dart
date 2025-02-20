d5c85c876e63e88b38889d030829a7a2039d3d0f
Luizmagao <luizmagao@gmail.com>
2025-02-20 18:31:44 -0300

    Questao 02 resolvida

--- lib/questao02.dart
+++ lib/questao02.dart
@@ -1,3 +1,14 @@
+import 'dart:io';
+
 void init() {
-  print('Here');
+  print('Digita a idade da primeira pessoa:');
+  var pessoa1 = int.parse(stdin.readLineSync()!);
+  print('Digita a idade da segunda pessoa:');
+  var pessoa2 = int.parse(stdin.readLineSync()!);
+  print('Digita a idade da terceira pessoa:');
+  var pessoa3 = int.parse(stdin.readLineSync()!);
+
+  var media = (pessoa1 + pessoa2 + pessoa3) / 3;
+
+  print('É média das idades é $media.');
 }
