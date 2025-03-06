f1c52cbddd94fca3450d858ba152f8a308661d43
Luiz Lins <luizmagao@gmail.com>
2025-03-06 09:40:18 -0300

    classe criada...

--- bin/name_project.dart
+++ bin/name_project.dart
@@ -2,16 +2,22 @@ import 'dart:io';
 
 void main() {
 
-  int diasDeColheita = 30;
   String fruta = "laranja";
+  String cor = "Verde e amarla";
+  String sabor = "Doce e cítrica";
+  double peso = 100.2;
+  int diasDeColheita = 30;
+  bool isModura = false;
 
-  mostrarMadura(fruta: fruta, dias: diasDeColheita);
 }
 
-mostrarMadura({String? fruta, int dias = 0}) {
-  if(dias >= 30) {
-    print("A $fruta está madura.");
-  } else {
-    print("A $fruta não está madura.");
-  }
+class fruta {
+  String fruta;
+  String cor;
+  String sabor;
+  double peso;
+  int diasDeColheita;
+  bool isModura;
+
+  Fruta(this.nome, this.cor, this.sabor, this.peso, this.diasDeColheita, {this.isModura});
 }
