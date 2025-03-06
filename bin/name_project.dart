727ad89d522a36c8dac30433af91f537eefe9e98
Luiz Lins <luizmagao@gmail.com>
2025-03-06 09:20:30 -0300

    posicional_obrigatorio

--- bin/name_project.dart
+++ bin/name_project.dart
@@ -2,48 +2,16 @@ import 'dart:io';
 
 void main() {
 
-  List<String> operacoes = <String>["+", "-", "/", "*"];
+  int diasDeColheita = 25;
+  String fruta = "laranja";
 
-  print('Informe um número: ');
-  var numeroUm = double.parse(stdin.readLineSync()!);
-  print('Informe outro número: ');
-  var numeroDois = double.parse(stdin.readLineSync()!);
-  print('Informe sua operação (+, -, /, *): ');
-  var operacao = stdin.readLineSync();
-  if(operacoes.contains(operacao)) {
-    print('operacao');
-  }
+  mostrarMadura(fruta, diasDeColheita);
+}
 
-  print('Resultado: ');
-  void somar() {
-    print(numeroUm + numeroDois);
-  }
-
-  void subtrair() {
-    print(numeroUm - numeroDois);
-  }
-
-  void multiplicacao() {
-    print(numeroUm * numeroDois);
-  }
-
-  void divisao() {
-    print(numeroUm / numeroDois);
-  }
-
-  if (operacao == '+') {
-    somar();
-  }
-
-  if (operacao == '-') {
-    subtrair();
-  }
-
-  if (operacao == '/') {
-    divisao();
-  }
-
-  if (operacao == '*') {
-    multiplicacao();
+mostrarMadura(String fruta, int dias) {
+  if(dias >= 30) {
+    print("A $fruta está madura.");
+  } else {
+    print("A $fruta não está madura.");
   }
 }
