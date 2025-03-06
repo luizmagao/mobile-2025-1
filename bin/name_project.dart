7174479d1c81d714192e61c27552b006e5034612
Luiz Lins <luizmagao@gmail.com>
2025-03-06 10:01:42 -0300

    usando métodos...

--- bin/name_project.dart
+++ bin/name_project.dart
@@ -6,14 +6,14 @@ void main() {
   String cor = "Verde e amarla";
   String sabor = "Doce e cítrica";
   double peso = 100.2;
-  int diasDeColheita = 30;
+  int diasDeColheita = 45;
   bool isModura = false;
 
   Fruta fruta1 = Fruta(fruta, cor, sabor, peso, diasDeColheita);
   Fruta fruta2 = Fruta("Uva", "roxa", "Doce", 20.9, 29);
 
   print(fruta1.fruta);
-  print(fruta1);
+  fruta1.estaMadura(40);
 
 }
 
@@ -26,4 +26,9 @@ class Fruta {
   bool isModura;
 
   Fruta(this.fruta, this.cor, this.sabor, this.peso, this.diasDeColheita, {this.isModura= false});
+
+  void estaMadura(int diasParaMadura){
+    this.isModura = this.diasDeColheita >= diasParaMadura;
+    print("A sua ${this.fruta} foi colhida a ${this.diasDeColheita} dias, e precisa de ${diasParaMadura}. Ela está ${this.isModura}.");
+  }
 }
