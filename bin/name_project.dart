d493911001de83abb50822894287f6ef1ba191c4
Luiz Lins <luizmagao@gmail.com>
2025-03-07 14:40:53 -0300

    Instancia Response criado

--- bin/name_project.dart
+++ bin/name_project.dart
@@ -7,5 +7,9 @@ void main() {
 
 void requestData() {
   String url = "https://gist.githubusercontent.com/luizmagao/161bd7cb770eb19256b8dc6055d53503/raw/cc10876ffca3ef619bda687dbba60054f7dc0961/accounts.json";
-  print(get(Uri.parse(url)));
+  Future<Response> futureResponse = get(Uri.parse(url));
+  print(futureResponse);
+  futureResponse.then((Response response) {
+    print(response);
+  });
 }
