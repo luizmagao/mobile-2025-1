9dfd93e352cba3a4b6fb2eefe52ba9cf62e65448
Luiz Lins <luizmagao@gmail.com>
2025-03-07 16:06:11 -0300

    Entendo sobre async e await

--- bin/name_project.dart
+++ bin/name_project.dart
@@ -24,3 +24,9 @@ void requestData() {
   print("Última coisa a acontecer na função");
 
 }
+
+requestDataAsync() async{
+  String url = "https://gist.githubusercontent.com/luizmagao/161bd7cb770eb19256b8dc6055d53503/raw/cc10876ffca3ef619bda687dbba60054f7dc0961/accounts.json";
+  Request request = await get(Uri.parse(url));
+  print(request);
+}
