69c86c1d5007751c997c7a3ad59c721e8fb148f6
Luiz Lins <luizmagao@gmail.com>
2025-03-07 14:31:29 -0300

    Instalando http e apresentando a url

--- bin/name_project.dart
+++ bin/name_project.dart
@@ -1,3 +1,10 @@
+import 'package:http/http.dart';
+
 void main() {
   print("Iniciando o http");
+}
+
+void requestData() {
+  String url = "https://gist.githubusercontent.com/luizmagao/161bd7cb770eb19256b8dc6055d53503/raw/cc10876ffca3ef619bda687dbba60054f7dc0961/accounts.json";
+  print(get(Uri.parse(url)));
 }
