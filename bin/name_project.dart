11104bec91434f62b8c63c67deb731f7df90d0fe
Luizmagao <luizmagao@gmail.com>
2025-03-07 23:41:58 -0300

    :sparkles: Realizando uma requisao

--- bin/name_project.dart
+++ bin/name_project.dart
@@ -42,5 +42,9 @@ void sendDataAsync(Map<String, dynamic> mapAccount) async {
   List<dynamic> listAccounts = await requestDataSync();
   listAccounts.add(mapAccount);
   String content = json.encode(listAccounts);
-  print(content);
+
+  String url =
+      "https://gist.githubusercontent.com/luizmagao/161bd7cb770eb19256b8dc6055d53503/raw/cc10876ffca3ef619bda687dbba60054f7dc0961/accounts.json";
+  Response response = await post(Uri.parse(url), body: content);
+  print(response.statusCode);
 }
