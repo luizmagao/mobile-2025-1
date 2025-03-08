103fd35b6295f2bea3f63c1529208a894995c8c2
Luizmagao <luizmagao@gmail.com>
2025-03-07 23:11:43 -0300

    Adicionando função sincrona.

--- bin/name_project.dart
+++ bin/name_project.dart
@@ -3,11 +3,12 @@ import 'dart:convert';
 
 void main() {
   print("Iniciando o http");
-  requestData();
+  requestDataSync();
 }
 
 void requestData() {
-  String url = "https://gist.githubusercontent.com/luizmagao/161bd7cb770eb19256b8dc6055d53503/raw/cc10876ffca3ef619bda687dbba60054f7dc0961/accounts.json";
+  String url =
+      "https://gist.githubusercontent.com/luizmagao/161bd7cb770eb19256b8dc6055d53503/raw/cc10876ffca3ef619bda687dbba60054f7dc0961/accounts.json";
   Future<Response> futureResponse = get(Uri.parse(url));
   print(futureResponse);
   futureResponse.then((Response response) {
@@ -16,17 +17,18 @@ void requestData() {
     List<dynamic> listAccounts = json.decode(response.body);
 
     Map<String, dynamic> mapCarla = listAccounts.firstWhere(
-      (element) => element["name"] == 'Carla'
+      (element) => element["name"] == 'Carla',
     );
-    
+
     print(mapCarla);
   });
   print("Última coisa a acontecer na função");
-
 }
 
-requestDataAsync() async{
-  String url = "https://gist.githubusercontent.com/luizmagao/161bd7cb770eb19256b8dc6055d53503/raw/cc10876ffca3ef619bda687dbba60054f7dc0961/accounts.json";
-  Request request = await get(Uri.parse(url));
-  print(request);
+requestDataSync() async {
+  String url =
+      "https://gist.githubusercontent.com/luizmagao/161bd7cb770eb19256b8dc6055d53503/raw/cc10876ffca3ef619bda687dbba60054f7dc0961/accounts.json";
+  Response request = await get(Uri.parse(url));
+  print(json.decode(request.body)[0]);
+  print("De fato, o final da função.");
 }
